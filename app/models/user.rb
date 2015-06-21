43bcb257ada3b4e9bcf55355eb589cdd4ee811f2
montekaka <josh.chen.jia@gmail.com>
2015-06-21 12:02:39 -0700

    hardcode email for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,17 +13,29 @@ class User < ActiveRecord::Base
 	  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
 	    user.provider = auth.provider
 	    user.uid = auth.uid	    
-	    user.oauth_token = auth.credentials.token
-	    user.save!
+	    user.oauth_token = auth.credentials.token	    
+	    user.email = auth.uid+"@facebook.com"
 	  end
 	end  
 
-	def self.new_with_session(params, seesion)
-		if seesion["devise.user_attributes"]
-			new(session["devise.user_attributes"], without_protection: true) do |use|
-				user.user_attributes = params
+	def self.new_with_session(params, session)
+		if session["devise.user_attributes"]
+			new(session["devise.user_attributes"], without_protection: true) do |user|
+				user.attributes = params
 				user.valid?
 			end
+		else
+			super
+		end
+	end
+
+	def password_required?
+		super && provider.blank?
+	end
+
+	def update_with_password(params, *options)
+		if encrypted_password.blank?
+			update_attributes(params, *options)
 		else
 			super
 		end
